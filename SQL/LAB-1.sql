9028b450792fe28dd355588d498a5558fbefccd0
Abdullah-crns <abdullah2023037@gmail.com>
2025-02-04 12:03:55 +0500

    updated

--- SQL/LAB-1.sql
+++ SQL/LAB-1.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE LAB1;
-CREATE TABLE LAB1 IF NOT EXISTS(
+CREATE TABLE credentials IF NOT EXISTS(
     user_id INT PRIMARY KEY,
     user_name VARCHAR(50) NOT NULL,
     user_email VARCHAR(50) NOT NULL,
@@ -8,3 +8,20 @@ CREATE TABLE LAB1 IF NOT EXISTS(
     created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 SELECT * FROM LAB1;
+
+CREATE TABLE roles(
+ role_id serial PRIMARY KEY,
+ role_name VARCHAR (255) UNIQUE NOT NULL
+);
+SELECT * FROM roles;
+
+CREATE TABLE account_roles (
+ user_id INT NOT NULL,
+ role_id INT NOT NULL,
+ grant_date TIMESTAMP,
+ PRIMARY KEY (user_id, role_id),
+ FOREIGN KEY (role_id)
+ REFERENCES roles (role_id),
+ FOREIGN KEY (user_id)
+ REFERENCES credentials (userid)
+);
