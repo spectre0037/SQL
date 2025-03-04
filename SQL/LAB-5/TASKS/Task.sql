755028168cdda80053f2f6c3a7edef8cdad8dbd9
Abdullah-crns <u2023037@giki.edu.pk>
2025-03-04 12:23:15 +0500

    updated task 5

--- SQL/LAB-5/TASKS/Task.sql
+++ SQL/LAB-5/TASKS/Task.sql
@@ -29,3 +29,83 @@ VALUES
 (113,'Avis',9000),
 (114,'Bernard',8000),
 (115,'Comte',20000)
+
+
+CREATE TABLE employees(
+eid INT PRIMARY KEY,
+ename VARCHAR(30),
+salary INT
+)
+
+INSERT INTO employees(eid,ename,salary)
+VALUES
+(100,'Oliver',85000),
+(101,'Jack',50000),
+(102,'Thomas',89000),
+(103,'George',10000),
+(105,'James',90000),
+(106,'Daneil',100000),
+(107,'Noah',50000),
+(108,'Joe',25000),
+(109,'Pheebs',90000),
+(110,'Ross',5000)
+
+
+INSERT INTO certified(eid,aid)
+VALUES
+(100,114),
+(102,113),
+(105,112),
+(106,115),
+(107,111),
+(109,112)
+
+
+SELECT eid,(SELECT aname FROM aircraft WHERE aid = c.aid) aircraft_name
+FROM certified c
+
+
+SELECT eid,(SELECT aname FROM aircraft WHERE aid = c.aid) aircraft_name,
+(SELECT ename FROM employees WHERE eid = c.eid) names
+FROM certified c
+
+
+
+SELECT ename
+FROM employees
+WHERE eid = (
+    SELECT eid 
+    FROM certified 
+    WHERE aid = (
+        SELECT aid 
+        FROM aircraft 
+        WHERE cruisingrange = (
+            SELECT MAX(cruisingrange) 
+            FROM aircraft
+        )
+    )
+);
+
+
+
+
+SELECT ename,salary
+FROM employees
+WHERE eid = (
+    SELECT eid 
+    FROM certified 
+    WHERE aid = (
+        SELECT aid 
+        FROM aircraft 
+        WHERE cruisingrange = (
+            SELECT MAX(cruisingrange) 
+            FROM aircraft
+        )
+    )
+);
+
+
+SELECT aname FROM aircraft
+WHERE cruisingrange >=(
+SELECT distance FROM flights WHERE from_loc = 'LA'
+)
